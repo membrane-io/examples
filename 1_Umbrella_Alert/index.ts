805dc279913851f5f720501e3447bea9fcf6185e
Juan Campa <juancampa@gmail.com>
2023-03-23 18:13:32 -0400

    Simplify code

--- 1_Umbrella_Alert/index.ts
+++ 1_Umbrella_Alert/index.ts
@@ -8,53 +8,64 @@ const BASE_URL = "http://api.openweathermap.org";
 export const Root = {
   async status() {
     if (!state.appId || !state.cityData) {
-      return "Please invoke the :setup action first.";
+      return "Please invoke the :configure action first.";
     }
     return "Ready";
   },
-  async setup({ args: { api_key, zip_code, country_code } }) {
-    // Retrieve information about the given zip code and country code
-    const response = await fetch(
-      `${BASE_URL}/geo/1.0/zip?zip=${zip_code},${country_code}&appid=${api_key}`
-    );
-    const data = await response.json();
+
+  async configure({ args: { apiKey, zipCode, countryCode } }) {
+    // Retrieve lat/lon for the given zip/country code
+    const params = new URLSearchParams({
+      zip: zipCode,
+      countryCode: countryCode,
+      appid: apiKey,
+    });
+    const res = await fetch(`${BASE_URL}/geo/1.0/zip?${params}`);
+
+    if (res.status !== 200) {
+      throw new Error(`Could not fetch city data: ${res.text()}`);
+    }
+
     // Save the retrieved city data and API key in the state object
-    state.cityData = data;
-    state.appId = api_key;
-    // run every day at 7 AM
-    await root.alert().$invokeAtCron(`0 0 7 * * *`)
+    state.cityData = await res.json();
+    state.apiKey = apiKey;
+
+    // Check every day at 7 AM
+    await root.checkWeather().$invokeAtCron(`0 0 7 * * *`);
   },
-  async alert() {
-    const { appId, cityData } = state;
-    // Getting the current date and hour
-    const currentDate = new Date();
-    const hours = currentDate.getHours();
-    // Setting the hour to 7 AM
-    const dt = currentDate.setHours(7);
-    const response = await fetch(
-      `${BASE_URL}/data/2.5/onecall?dt=${dt}&lat=${cityData.lat}&lon=${cityData.lon}&exclude=,minutely,daily&appid=${appId}`
-    );
-    const data = await response.json();
-    const forecastValues = data.hourly.slice(0, 24);
-    const remainingHours = 24 - hours;
-    // Forecast data to only include the remaining hours in the day
-    const valuesForToday = forecastValues.slice(0, remainingHours);
-    // Looping through the hourly forecast data to check for rain
-    let rainForecastFound = false;
-    for (const hourly of valuesForToday) {
-      for (const weather of hourly.weather) {
-        // If it's going to rain, send an SMS message and set the rain forecast flag to true
-        if (weather.id >= 500 && weather.id <= 531) {
-          const message = `It's going to rain today. Don't forget your umbrella! ☔️`;
-          // send sms with the alert.
-          await nodes.sms.send({ message });
-          rainForecastFound = true;
-          break;
-        }
-      }
-      if (rainForecastFound) {
-        break;
-      }
+
+  async checkWeather() {
+    // Fetch the weather data for our location
+    // More info: https://openweathermap.org/api/one-call-api
+    const params = new URLSearchParams({
+      dt: Date.now().toString(),
+      lat: state.cityData.lat,
+      lon: state.cityData.lon,
+      exclude: "minutely,daily",
+      appid: state.apiKey,
+    });
+    const res = await fetch(`${BASE_URL}/data/2.5/onecall?${params}`);
+
+    if (res.status !== 200) {
+      throw new Error(`Could not fetch weather data: ${res.text()}`);
+    }
+
+    // Will it rain at any point between now and 8 PM? (in the next 13h)
+    const body = await res.json();
+    const rainToday = body.hourly
+      .slice(0, 13)
+      .flatMap((hour) => hour.weather)
+      .some(isRainyWeather);
+
+    if (rainToday) {
+      const message = `☔ It'll rain today. Don't forget your umbrella!`;
+      await nodes.sms.send({ message });
     }
   },
 };
+
+// Anything between 200 and 600 is considered "rainy"
+// See https://openweathermap.org/weather-conditions
+function isRainyWeather(weather) {
+  return weather.id >= 200 && weather.id < 600;
+}
